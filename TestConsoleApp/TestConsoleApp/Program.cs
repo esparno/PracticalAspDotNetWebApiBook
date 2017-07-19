46233016456896dfb3207722de50f183882070c4
esparno <elsparno@gmail.com>
2017-07-19 12:21:24 -0700

    test console app for capturing traffic through fiddler

---
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "http://localhost.fiddler:49741/api/employees";
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadString(uri);
+            }
+        }
+    }
+}
