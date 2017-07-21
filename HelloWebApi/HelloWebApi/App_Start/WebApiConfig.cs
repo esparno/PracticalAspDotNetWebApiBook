2d58a15e5aecd0925cb9163f2d1ce27a9c0d270a
esparno <elsparno@gmail.com>
2017-07-20 20:02:37 -0700

    HelloWebApi WebApiTracer

--- HelloWebApi/HelloWebApi/App_Start/WebApiConfig.cs
+++ HelloWebApi/HelloWebApi/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Tracing;
 
 namespace HelloWebApi
 {
@@ -15,6 +16,8 @@ namespace HelloWebApi
                 defaults: new { id = RouteParameter.Optional }
             );
 
+            config.EnableSystemDiagnosticsTracing();
+            config.Services.Replace(typeof(ITraceWriter), new WebApiTracer());
             // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
             // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
             // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
@@ -22,7 +25,7 @@ namespace HelloWebApi
 
             // To disable tracing in your application, please comment out or remove the following line of code
             // For more information, refer to: http://www.asp.net/web-api
-            config.EnableSystemDiagnosticsTracing();
+            //config.EnableSystemDiagnosticsTracing();
         }
     }
 }
---
+++ HelloWebApi/HelloWebApi/WebApiTracer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web.Http.Tracing;
+using System.Net.Http;
+using System.Xml;
+using System.Text;
+using System.IO;
+
+namespace HelloWebApi
+{
+    public class WebApiTracer : ITraceWriter
+    {
+        public void Trace(HttpRequestMessage request, string category, TraceLevel level, Action<TraceRecord> traceAction)
+        {
+            if (level != TraceLevel.Off)
+            {
+                TraceRecord rec = new TraceRecord(request, category, level);
+                traceAction(rec);
+                WriteXmlElement(rec);
+            }
+        }
+        private void WriteXmlElement(TraceRecord rec)
+        {
+            using (Stream xmlFile = new FileStream(@"C:\path/log.xml", FileMode.Append))
+            {
+                using (XmlTextWriter writer = new XmlTextWriter(xmlFile, Encoding.UTF8))
+                {
+                    writer.Formatting = Formatting.Indented;
+                    writer.WriteStartElement("trace");
+                    writer.WriteElementString("timestamp", rec.Timestamp.ToString());
+                    writer.WriteElementString("operation", rec.Operation);
+                    writer.WriteElementString("user", rec.Operator);
+
+                    if (!string.IsNullOrWhiteSpace(rec.Message))
+                    {
+                        writer.WriteStartElement("message");
+                        writer.WriteCData(rec.Message);
+                        writer.WriteEndElement();
+                    }
+
+                    writer.WriteElementString("category", rec.Category);
+                    writer.WriteEndElement();
+                    writer.WriteString(Environment.NewLine);
+                    writer.Flush();
+                }
+            }
+        }
+    }
+}
