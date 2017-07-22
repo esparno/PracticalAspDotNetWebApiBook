438ecd8b493c723386c0f391e4eb6969879404e9
esparno <elsparno@gmail.com>
2017-07-22 10:17:41 -0700

    HelloWebApi map to custom media type

--- HelloWebApi/HelloWebApi/App_Start/WebApiConfig.cs
+++ HelloWebApi/HelloWebApi/App_Start/WebApiConfig.cs
@@ -28,7 +28,8 @@ namespace HelloWebApi
             //config.Formatters.RemoveAt(0);
             //config.Formatters.JsonFormatter.MediaTypeMappings.Add(new QueryStringMapping("frmt", "json", new MediaTypeHeaderValue("application/json")));
             //config.Formatters.XmlFormatter.MediaTypeMappings.Add(new QueryStringMapping("frmt", "xml", new MediaTypeHeaderValue("application/xml")));
-            config.Formatters.JsonFormatter.MediaTypeMappings.Add(new RequestHeaderMapping("X-Media", "json", StringComparison.OrdinalIgnoreCase, false, new MediaTypeHeaderValue("application/json")));
+            //config.Formatters.JsonFormatter.MediaTypeMappings.Add(new RequestHeaderMapping("X-Media", "json", StringComparison.OrdinalIgnoreCase, false, new MediaTypeHeaderValue("application/json")));
+            config.Formatters.JsonFormatter.MediaTypeMappings.Add(new IPBasedMediaTypeMapping());
             foreach (var formatter in config.Formatters)
             {
                 Trace.WriteLine(formatter.GetType().Name);
---
+++ HelloWebApi/HelloWebApi/IPBasedMediaTypeMapping.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel.Channels;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+
+namespace HelloWebApi
+{
+    public class IPBasedMediaTypeMapping : MediaTypeMapping
+    {
+        public IPBasedMediaTypeMapping() : base(new MediaTypeHeaderValue("application/json")) { }
+        public override double TryMatchMediaType(HttpRequestMessage request)
+        {
+            string ipAddress = String.Empty;
+            if (request.Properties.ContainsKey("MS_HttpContext"))
+            {
+                var httpContext = (HttpContextBase)request.Properties["MS_HttpContext"];
+                ipAddress = httpContext.Request.UserHostAddress;
+            }
+            else if (request.Properties.ContainsKey(RemoteEndpointMessageProperty.Name))
+            {
+                RemoteEndpointMessageProperty prop;
+                prop = (RemoteEndpointMessageProperty) request.Properties[RemoteEndpointMessageProperty.Name];
+                ipAddress = prop.Address;
+            }
+            return "::1".Equals(ipAddress) ? 1.0 : 0.0;
+        }
+    }
+}
