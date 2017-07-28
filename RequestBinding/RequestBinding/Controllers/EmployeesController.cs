4d6f0d73a226b861e545174f1169ce931892a303
esparno <elsparno@gmail.com>
2017-07-28 13:45:30 -0600

    read raw request content

---
+++ RequestBinding/RequestBinding/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Diagnostics;
+
+namespace RequestBinding.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public void Post(HttpRequestMessage request)
+        {
+            var content = request.Content.ReadAsStringAsync().Result;
+            int id = Int32.Parse(request.RequestUri.Segments.Last());
+
+            Trace.WriteLine(content);
+            Trace.WriteLine(id);
+        }
+    }
+}
