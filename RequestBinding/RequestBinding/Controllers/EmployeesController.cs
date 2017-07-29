8adb640cfb5291f920aa7a679d339332ed5de380
esparno <elsparno@gmail.com>
2017-07-28 19:51:23 -0600

    RequestBinding binding http request to simple types

--- RequestBinding/RequestBinding/Controllers/EmployeesController.cs
+++ RequestBinding/RequestBinding/Controllers/EmployeesController.cs
@@ -5,18 +5,37 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Diagnostics;
+using RequestBinding.Models;
 
 namespace RequestBinding.Controllers
 {
     public class EmployeesController : ApiController
     {
-        public void Post(HttpRequestMessage request)
-        {
-            var content = request.Content.ReadAsStringAsync().Result;
-            int id = Int32.Parse(request.RequestUri.Segments.Last());
+        //public void Post(int id, Employee employee)
+        //{
+        //    //var content = request.Content.ReadAsStringAsync().Result;
+        //    //var content = req.Content.ReadAsAsync<Employee>().Result;
+        //    //int id = Int32.Parse(req.RequestUri.Segments.Last());
 
-            Trace.WriteLine(content);
+        //    //Trace.WriteLine(content.Id);
+        //    //Trace.WriteLine(content.FirstName);
+        //    //Trace.WriteLine(content.LastName);
+        //    //Trace.WriteLine(id);
+
+
+        //    Trace.WriteLine(employee.Id);
+        //    Trace.WriteLine(employee.FirstName);
+        //    Trace.WriteLine(employee.LastName);
+        //    Trace.WriteLine(id);
+        //}
+
+        // Binding an Http Request to Simple Types
+        public void Post(int id, string firstName, [FromBody]int locationid, Guid guid)
+        {
             Trace.WriteLine(id);
+            Trace.WriteLine(firstName);
+            Trace.WriteLine(locationid);
+            Trace.WriteLine(guid);
         }
     }
 }
