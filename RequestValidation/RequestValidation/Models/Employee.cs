8ad70909375aac5a06fefd0d3afb4e501b41bb04
esparno <elsparno@gmail.com>
2017-07-29 22:51:34 -0600

    RequestValidation validation with data annotations

---
+++ RequestValidation/RequestValidation/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RequestValidation.Models
+{
+    public class Employee
+    {
+        [Range(10000, 90000)]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        public int Department { get; set; }
+    }
+}
