185d1dbe58d8fd9e911f773bfad624b1e454bcbb
esparno <elsparno@gmail.com>
2017-07-30 20:33:07 -0600

    TalentManager loosely coupling db with repository pattern

---
+++ TalentManager/TalentManager.Data/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using TalentManager.Domain;
+
+namespace TalentManager.Data
+{
+    public interface IEmployeeRepository :IDisposable
+    {
+        IEnumerable<Employee> GetByDepartment(int departmentId);
+        Employee Get(int id);
+    }
+}
--- TalentManager/TalentManager.Web/Controllers/EmployeesController.cs
+++ TalentManager/TalentManager.Web/Controllers/EmployeesController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net;
 using System.Web.Http;
 using System.Net.Http;
-using System.Web.Http;
 using TalentManager.Data;
 using TalentManager.Domain;
 
@@ -12,34 +11,96 @@ namespace TalentManager.Web.Controllers
 {
     public class EmployeesController : ApiController
     {
-        private IContext context = null;
+        private readonly IEmployeeRepository repository = null;
 
         public EmployeesController()
         {
-            context = new Context();
+            this.repository = new EmployeeRepository();
         }
-        public EmployeesController(IContext context)
+
+        public EmployeesController(IEmployeeRepository repository)
         {
-            this.context = context;
+            this.repository = repository;
         }
+
         public HttpResponseMessage Get(int id)
         {
-            var employee = context.Employees.FirstOrDefault(e => e.Id == id);
-            if(employee == null)
+            var employee = repository.Get(id);
+            if (employee == null)
             {
-                var response = Request.CreateResponse(HttpStatusCode.NotFound, "Employee Not Found");
+                var response = Request.CreateResponse(HttpStatusCode.NotFound, "Employee not found");
+
                 throw new HttpResponseException(response);
             }
+
             return Request.CreateResponse<Employee>(HttpStatusCode.OK, employee);
+        }
+
+        public HttpResponseMessage GetByDepartment(int departmentId)
+        {
+            var employees = repository.GetByDepartment(departmentId);
+            if (employees != null && employees.Any())
+            {
+                return Request.CreateResponse<IEnumerable<Employee>>(HttpStatusCode.OK, employees);
+            }
+
+            throw new HttpResponseException(HttpStatusCode.NotFound);
         }
 
         protected override void Dispose(bool disposing)
         {
-            if (context != null && context is IDisposable)
-            {
-                ((IDisposable)context).Dispose();
-            }
+            if (repository != null)
+                repository.Dispose();
+
             base.Dispose(disposing);
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //private IContext context = null;
+
+        //public EmployeesController()
+        //{
+        //    context = new Context();
+        //}
+        //public EmployeesController(IContext context)
+        //{
+        //    this.context = context;
+        //}
+        //public HttpResponseMessage Get(int id)
+        //{
+        //    var employee = context.Employees.FirstOrDefault(e => e.Id == id);
+        //    if(employee == null)
+        //    {
+        //        var response = Request.CreateResponse(HttpStatusCode.NotFound, "Employee Not Found");
+        //        throw new HttpResponseException(response);
+        //    }
+        //    return Request.CreateResponse<Employee>(HttpStatusCode.OK, employee);
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (context != null && context is IDisposable)
+        //    {
+        //        ((IDisposable)context).Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
     }
 }
