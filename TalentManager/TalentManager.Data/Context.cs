1bc4ed284ffe1c4a5ae963e9d00a219821bcf2a6
esparno <elsparno@gmail.com>
2017-07-30 18:44:54 -0600

    TalentManager connect to sql server express db

---
+++ TalentManager/TalentManager.Data/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+using TalentManager.Data.Configuration;
+using TalentManager.Domain;
+
+namespace TalentManager.Data
+{
+    public class Context : DbContext
+    {
+        public Context() : base("DefaultConnection") { }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Department { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Configurations.Add(new EmployeeConfiguration()).Add(new DepartmentConfiguration());
+        }
+    }
+}
---
+++ TalentManager/TalentManager.Domain/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalentManager.Domain
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int DepartmentId { get; set; }
+        public virtual Department Department { get; set; }
+        public byte[] RowVersion { get; set; }
+    }
+}
