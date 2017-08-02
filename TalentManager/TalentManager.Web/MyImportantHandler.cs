60439b9cdfa4830729b033319095fa1e114306e3
esparno <elsparno@gmail.com>
2017-08-01 19:55:02 -0600

    TalentManager creating a message handler

---
+++ TalentManager/TalentManager.Web/MyImportantHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TalentManager.Web
+{
+    public class MyImportantHandler : DelegatingHandler
+    {
+        private const string REQUEST_HEADER = "X-Name";
+        private const string RESPONSE_HEADER = "X-Message";
+        private const string NAME = "Voldemort";
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            string name = String.Empty;
+
+            if (request.Headers.Contains(REQUEST_HEADER))
+            {
+                name = request.Headers.GetValues(REQUEST_HEADER).First();
+            }
+
+            if (NAME.Equals(name, StringComparison.OrdinalIgnoreCase))
+                return request.CreateResponse(HttpStatusCode.Forbidden);
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if (response.StatusCode == HttpStatusCode.OK &&
+                                        !String.IsNullOrEmpty(name))
+            {
+                response.Headers.Add(RESPONSE_HEADER, String.Format("Hello, {0}. Time is {1}", name,
+                                    DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt")));
+            }
+
+            return response;
+        }
+    }
+}
---
+++ TalentManager/TalentManager.Web/MyNotSoImportantHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TalentManager.Web
+{
+    public class MyNotSoImportantHandler : DelegatingHandler
+    {
+        private const string REQUEST_HEADER = "X-Name2";
+        private const string RESPONSE_HEADER = "X-Message2s";
+        private const string NAME = "Potter";
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            string name = String.Empty;
+
+            if (request.Headers.Contains(REQUEST_HEADER))
+            {
+                name = request.Headers.GetValues(REQUEST_HEADER).First();
+            }
+
+            if (NAME.Equals(name, StringComparison.OrdinalIgnoreCase))
+                return request.CreateResponse(HttpStatusCode.Forbidden);
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if (response.StatusCode == HttpStatusCode.OK &&
+                                        !String.IsNullOrEmpty(name))
+            {
+                response.Headers.Add(RESPONSE_HEADER, String.Format("Hello, {0}. Time is {1}", name,
+                                    DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt")));
+            }
+
+            return response;
+        }
+    }
+}
