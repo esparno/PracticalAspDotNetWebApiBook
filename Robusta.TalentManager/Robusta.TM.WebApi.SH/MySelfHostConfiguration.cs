873c3811d97a3daadf268c7421465e3fe2a335e7
esparno <elsparno@gmail.com>
2017-08-03 21:16:54 -0600

    RobustaTalentManager self hosting

---
+++ Robusta.TalentManager/Robusta.TM.WebApi.SH/MySelfHostConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel.Channels;
+using System.Web.Http.SelfHost;
+using System.Web.Http.SelfHost.Channels;
+
+namespace Robusta.TM.WebApi.SH
+{
+    public class MySelfHostConfiguration : HttpSelfHostConfiguration
+    {
+        public MySelfHostConfiguration(string baseAddress) : base(baseAddress) { }
+
+        protected override BindingParameterCollection OnConfigureBinding(HttpBinding httpBinding)
+        {
+            httpBinding.Security.Mode = HttpBindingSecurityMode.Transport;
+
+            return base.OnConfigureBinding(httpBinding);
+        }
+    }
+}
---
+++ Robusta.TalentManager/Robusta.TM.WebApi.SH/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Http.SelfHost;
+using Robusta.TalentManager.WebApi.Core.Configuration;
+
+namespace Robusta.TM.WebApi.SH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var configuration = new HttpSelfHostConfiguration("http://localhost:8086");
+            var configuration = new MySelfHostConfiguration("https://localhost:8086");
+
+            WebApiConfig.Register(configuration);
+            DtoMapperConfig.CreateMaps();
+            IocConfig.RegisterDependencyResolver(configuration);
+
+            using (HttpSelfHostServer server = new HttpSelfHostServer(configuration))
+            {
+                server.OpenAsync().Wait();
+                Console.WriteLine("Press Enter to terminate the server...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
